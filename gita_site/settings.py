9188baf178e2c42cfcce94ae826c86ff62e21df5
Sneh Harsh <snehharsh@Snehs-MacBook-Air.local>
2026-04-12 01:50:54 +0530

    Loading static files on GCP server

--- gita_site/settings.py
+++ gita_site/settings.py
@@ -130,7 +130,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [BASE_DIR / 'accounts' / 'static']
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
